7c171c422431b9c48037a55cfb2ea7d8ea68c20d
Luka Kršul <luka.k007@gmail.com>
2024-10-17 10:34:12 +0200

    added comments

--- database/seeders/LanguagesTableSeeder.php
+++ database/seeders/LanguagesTableSeeder.php
@@ -16,7 +16,7 @@ class LanguagesTableSeeder extends Seeder
         // Manually insert languages
         DB::table('languages')->insert([
             ['locale' => 'en_GB', 'language' => 'English'],
-            ['locale' => 'hr_HR', 'language' => 'Hrvatski (Croatian)'],
+            ['locale' => 'hr_HR', 'language' => 'Hrvatski (Croatian)'],  //Unfortunately, method -> realText() does not support Croatian language, so the return text will be in English
             ['locale' => 'de_DE', 'language' => 'Deutsch (German)'],
         ]);
     }
